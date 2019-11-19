97b468a10b65d7eba829c309db3cabd2ccdfb088
lovinix <sylph0606@gmail.com>
2019-11-19 23:13:44 +0900

    이진수

---
+++ 2226.cpp
@@ -0,0 +1,15 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	cin >> s;
+	if (s.back() == ' ') return -1;
+	return 0;
+}
