bbbd041732e27ed6cd90a16a101a38d016e5a66f
lovinix <sylph0606@gmail.com>
2019-11-19 16:02:50 +0900

    피자는 나눌 수록 커지잖아요

---
+++ 17946.cpp
@@ -0,0 +1,17 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, K;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	cin >> N;
+	while (N--)
+	{
+		cout << "1\n";
+	}
+}
